363f7e074c3236e732f3432bcab229495a15e08f
Pankaj Ghosh <pankaj.ghosh@gmail.com>
2014-08-14 22:15:11 -0400

    got posters from imdb and local cachehe working

--- web.go
+++ web.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+    "net/http"
     "io/ioutil"
     "github.com/gin-gonic/gin"
+    "encoding/json"
+    "strings"
+    "fmt"
 )
+
+var global_img_cache = make(map[string][]map[string]interface{});
 
 func main() {
     r := gin.Default()
-    r.GET("/", hello)
+    r.GET("/", root)
     r.GET("/ping", ping)
+    r.GET("/links/:title", links)
 
     r.Run(":8080")
 }
 
-func hello(c *gin.Context) {
-    content, err := ioutil.ReadFile("main.html")
-    if err != nil {
-        //Do something
-    }
-    c.Data(200, "text/html", content)
+func root(c *gin.Context) {
+    c.File("root.html")
 }
 
 func ping(c *gin.Context) {
-    c.String(200, "Pong")
+    c.JSON(200, gin.H{"ping":"pong"})
+}
+
+func links(c *gin.Context) {
+    title := c.Params.ByName("title")
+    var data []map[string]interface{}
+
+    if _, key_exists_in_cache := global_img_cache[title]; key_exists_in_cache{
+        fmt.Println("using cache")
+        data = global_img_cache[title]
+    }else{
+        fmt.Println("cache miss. going to db")
+        s := []string{"http://www.myapifilms.com/imdb?title=", title, "&exactFilter=0&limit=10"}
+        fmt.Println(strings.Join(s, ""))
+        resp, _ := http.Get(strings.Join(s, ""))
+
+        defer resp.Body.Close()
+        body, _ := ioutil.ReadAll(resp.Body)
+
+        if err := json.Unmarshal(body, &data); err != nil {
+            panic(err)
+        }
+
+        global_img_cache[title] = data
+    }
+    c.JSON(200, data)    
 }
